e37e0c549ecbeb6a58939b4cc47accf6c0dcf063
rishithamenush <rishitha.menush@gmail.com>
2025-06-24 12:28:32 +0530

    update the UI

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  isSignUp = true;
 
+  setAuthMode(signUp: boolean) {
+    this.isSignUp = signUp;
+  }
 }
