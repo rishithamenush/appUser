61fe4027f89ef5b75b3a240a34222720ba43d931
rishithamenush <rishitha.menush@gmail.com>
2025-06-24 13:00:43 +0530

    add the ngMoel for the form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import {NgIf, NgClass} from '@angular/common';
+import {UserRegister} from '../../model/user.model';
+import {FormsModule, NgModel} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   imports: [
     NgIf,
-    NgClass
+    NgClass,
+    FormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
@@ -13,6 +16,7 @@ import {NgIf, NgClass} from '@angular/common';
 export class LoginComponent {
   isSignUp = true;
   animationClass = 'signupAnimation';
+  registerObj: UserRegister = new UserRegister();
 
   setAuthMode(signUp: boolean) {
     if (signUp !== this.isSignUp) {
