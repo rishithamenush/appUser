7be4e09172360e669e1d97c062d96818ec531f41
rishithamenush <rishitha.menush@gmail.com>
2025-06-24 12:43:07 +0530

    login animation

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
-import {NgIf} from '@angular/common';
+import {NgIf, NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-login',
   imports: [
-    NgIf
+    NgIf,
+    NgClass
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   isSignUp = true;
+  animationClass = 'signupAnimation';
 
   setAuthMode(signUp: boolean) {
-    this.isSignUp = signUp;
+    if (signUp !== this.isSignUp) {
+      this.animationClass = signUp ? 'signupAnimation' : 'loginAnimation';
+      this.isSignUp = signUp;
+    }
   }
 }
