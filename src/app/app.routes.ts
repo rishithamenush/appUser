15bda3ee3f3017889240fae5ddd4b9efc838b8ca
rishithamenush <rishitha.menush@gmail.com>
2025-06-25 10:49:35 +0530

    add the app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from './pages/login/login.component';
 import {LayoutComponent} from './pages/layout/layout.component';
 import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {AccountComponent} from './pages/account/account.component';
 
 export const routes: Routes = [
   {
@@ -22,6 +23,10 @@ export const routes: Routes = [
       {
         path: 'dashboard',
         component: DashboardComponent,
+      },
+      {
+        path: 'account',
+        component: AccountComponent,
       }
     ]
   }
